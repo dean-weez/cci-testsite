5cf3bc249ab6509839583fa211a32aad016d4942
dean-weez <dean.weesner@gmail.com>
2025-05-06 21:04:19 -0700

    bug stuff

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("psssst secret h@ck3r message");
+
+let bucket = document.getElementById("bucket");
+let bug = document.getElementById("bug");
+let infoText = document.getElementById("info-text");
+
+bucket.addEventListener("click", () => {
+  bucket.style.transition = 'transform 1s ease-in-out';
+  bucket.style.transform = 'rotate(180deg)';
+
+  setTimeout(() => {
+    bug.style.transition = 'transform 1s ease-in-out';
+    bug.style.transform = 'translate(0, 300px)';
+  }, 1000);
+});
+
